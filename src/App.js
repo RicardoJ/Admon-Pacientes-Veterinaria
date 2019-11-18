1576b7f1aff1c155b241dbd5bcf9bfa1f5d47fba
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-11-18 15:48:00 -0500

    adding form with styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import "./bootstrap.min.css";
 import Header from "./components/Header";
+import NewDate from "./components/NewDate";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div class="container">
+      <div className="container">
         <Header titulo="Administrador paciente veterinaria" />
+
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NewDate />
+          </div>
+        </div>
       </div>
     );
   }
