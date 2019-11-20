bfb303ddb1deca8510ea03202bf8c161aab0cd25
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-11-20 11:57:06 -0500

    message info about dates

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Appointment  from './Appointment';
 import PropTypes from 'prop-types';
 const ListAppointment = ({appointments, deleteDate}) => {
+
+    const message = Object.keys(appointments).length === 0 ? 'No hay citas agendadas' : ' Administración de citas'
     return ( 
         <div className = "card mt-2 py-5">
             <div className = "card-body">
-                <h2 className = "card-title text-center">Administración de citas</h2>
+                <h2 className = "card-title text-center">{message}</h2>
                 <div className = "lista-citas">
                     {appointments.map(appointment =>(
                         <Appointment
